8a54c02d85e96fa0815b2e8bd13115fdffa89cd1
moskovanton <48486058+moskovanton@users.noreply.github.com>
2019-10-04 16:48:46 +0300

    Update ProductCode.java

--- src/ru/avalon/java/ocpjp/labs/ProductCode.java
+++ src/ru/avalon/java/ocpjp/labs/ProductCode.java
@@ -1,258 +1,101 @@
 package ru.avalon.java.ocpjp.labs;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.sql.DriverManager;
 import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
+import java.util.Properties;
 
 /**
- * Класс описывает представление о коде товара и отражает соответствующую 
- * таблицу базы данных Sample (таблица PRODUCT_CODE).
- * 
+ * Лабораторная работа №3
+ * <p>
+ * Курс: "DEV-OCPJP. Подготовка к сдаче сертификационных экзаменов серии Oracle Certified Professional Java Programmer"
+ * <p>
+ * Тема: "JDBC - Java Database Connectivity" 
+ *
  * @author Daniel Alpatov <danial.alpatov@gmail.com>
  */
-public class ProductCode {
+public class Main {
+
     /**
-     * Код товара
-     */
-    private String code;
-    /**
-     * Кода скидки
-     */
-    private char discountCode;
-    /**
-     * Описание
-     */
-    private String description;
-    /**
-     * Основной конструктор типа {@link ProductCode}
+     * Точка входа в приложение
      * 
-     * @param code код товара
-     * @param discountCode код скидки
-     * @param description описание 
+     * @param args the command line arguments
      */
-    public ProductCode(String code, char discountCode, String description) {
-        this.code = code;
-        this.discountCode = discountCode;
-        this.description = description;
-    }
-    /**
-     * Инициализирует объект значениями из переданного {@link ResultSet}
-     * 
-     * @param set {@link ResultSet}, полученный в результате запроса, 
-     * содержащего все поля таблицы PRODUCT_CODE базы данных Sample.
-     */
-    private ProductCode(ResultSet set) throws SQLException {
+    public static void main(String[] args) throws SQLException {
         /*
-         * TODO #05 реализуйте конструктор класса ProductCode
+         * TODO #01 Подключите к проекту все библиотеки, необходимые для соединения с СУБД.
          */
-        code = set.getString("prod_code");
-        discountCode = set.getString("discount_code").charAt(0);
-        description = set.getString("description");        
+        try (Connection connection = getConnection()) {
+            ProductCode code = new ProductCode("MO", 'N', "Movies");
+            
+            code.save(connection);
+            printAllCodes(connection);
+            
+            System.out.println("debug");
+
+            code.setCode("MV");
+            code.save(connection);
+            printAllCodes(connection);
+        }
+        /*
+         * TODO #14 Средствами отладчика проверьте корректность работы программы
+         */
     }
     /**
-     * Возвращает код товара
+     * Выводит в кодсоль все коды товаров
      * 
-     * @return Объект типа {@link String}
-     */
-    public String getCode() {
-        return code;
+     * @param connection действительное соединение с базой данных
+     * @throws SQLException 
+     */    
+    private static void printAllCodes(Connection connection) throws SQLException {
+        Collection<ProductCode> codes = ProductCode.all(connection);
+        for (ProductCode code : codes) {
+            System.out.println(code);
+        }
     }
     /**
-     * Устанавливает код товара
+     * Возвращает URL, описывающий месторасположение базы данных
      * 
-     * @param code код товара
+     * @return URL в виде объекта класса {@link String}
      */
-    public void setCode(String code) {
-        this.code = code;
+    private static String getUrl() {
+        /*
+         * TODO #02 Реализуйте метод getUrl
+         */
+        return getProperties().getProperty("url");
+    }
+    /**
+     * Возвращает параметры соединения
+     * 
+     * @return Объект класса {@link Properties}, содержащий параметры user и 
+     * password
+     */
+    private static Properties getProperties() {
+        /*
+         * TODO #03 Реализуйте метод getProperties
+         */
+        Properties pr = new Properties();
+        try (InputStream is = ClassLoader.getSystemResourceAsStream("resources\\db.properties")) {
+            pr.load(is);
+        } catch (IOException e){
+            System.err.println("Файл не найден!");
+          }
+        return pr;
+    }
+    /**
+     * Возвращает соединение с базой данных Sample
+     * 
+     * @return объект типа {@link Connection}
+     * @throws SQLException 
+     */
+    private static Connection getConnection() throws SQLException {
+        /*
+         * TODO #04 Реализуйте метод getConnection
+         */
+        return DriverManager.getConnection(getUrl(), getProperties());
     }
     
-    /**
-     * Возвращает код скидки
-     * 
-     * @return Объект типа {@link String}
-     */
-    public char getDiscountCode() {
-        return discountCode;
-    }
-    /**
-     * Устанавливает код скидки
-     * 
-     * @param discountCode код скидки
-     */
-    public void setDiscountCode(char discountCode) {
-        this.discountCode = discountCode;
-    }
-    /**
-     * Возвращает описание
-     * 
-     * @return Объект типа {@link String}
-     */
-    public String getDescription() {
-        return description;
-    }
-    /**
-     * Устанавливает описание
-     * 
-     * @param description описание
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    /**
-     * Хеш-функция типа {@link ProductCode}.
-     * 
-     * @return Значение хеш-кода объекта типа {@link ProductCode}
-     */
-    @Override
-    public int hashCode() {
-        /*
-         * TODO #06 Реализуйте метод hashCode
-         */
-        return code.hashCode() + discountCode + description.hashCode();
-    }
-    /**
-     * Сравнивает некоторый произвольный объект с текущим объектом типа 
-     * {@link ProductCode}
-     * 
-     * @param obj Объект, скоторым сравнивается текущий объект.
-     * @return true, если объект obj тождественен текущему объекту. В обратном 
-     * случае - false.
-     */
-    @Override
-    public boolean equals(Object obj) {
-        /*
-         * TODO #07 Реализуйте метод equals
-         */
-        if (this == obj) return true;
-        if (obj == null || obj.getClass() != getClass()) return false;
-        final ProductCode pc = (ProductCode) obj;
-        return Objects.equals(this, pc);
-    }
-    /**
-     * Возвращает строковое представление кода товара.
-     * 
-     * @return Объект типа {@link String}
-     */
-    @Override
-    public String toString() {
-        /*
-         * TODO #08 Реализуйте метод toString
-         */
-        return "Product code: " + code + " , discount code: " + discountCode + " , description: " + description + " .";
-    }
-    /**
-     * Возвращает запрос на выбор всех записей из таблицы PRODUCT_CODE 
-     * базы данных Sample
-     * 
-     * @param connection действительное соединение с базой данных
-     * @return Запрос в виде объекта класса {@link PreparedStatement}
-     * @throws java.sql.SQLException
-     */
-    public static PreparedStatement getSelectQuery(Connection connection) throws SQLException {
-        /*
-         * TODO #09 Реализуйте метод getSelectQuery
-         */
-        if (connection == null) throw new IllegalArgumentException("No connection");
-        String selectQuery = "SELECT * FROM PRODUCT_CODE";
-        return connection.prepareStatement(selectQuery);
-    }
-    /**
-     * Возвращает запрос на добавление записи в таблицу PRODUCT_CODE 
-     * базы данных Sample
-     * 
-     * @param connection действительное соединение с базой данных
-     * @return Запрос в виде объекта класса {@link PreparedStatement}
-     * @throws java.sql.SQLException
-     */
-    public static PreparedStatement getInsertQuery(Connection connection) throws SQLException {
-        /*
-         * TODO #10 Реализуйте метод getInsertQuery
-         */
-        if (connection == null) throw new IllegalArgumentException("No connection");
-        String insertQuery = "INSERT INTO PRODUCT_CODE (PROD_CODE, DISCOUNT_CODE, DESCRIPTION) values (?, ?, ?)";
-        return connection.prepareStatement(insertQuery);
-    }
-    /**
-     * Возвращает запрос на обновление значений записи в таблице PRODUCT_CODE 
-     * базы данных Sample
-     * 
-     * @param connection действительное соединение с базой данных
-     * @return Запрос в виде объекта класса {@link PreparedStatement}
-     * @throws java.sql.SQLException
-     */
-    public static PreparedStatement getUpdateQuery(Connection connection) throws SQLException {
-        /*
-         * TODO #11 Реализуйте метод getUpdateQuery
-         */
-        if (connection == null) throw new IllegalArgumentException("No connection");
-        String updateQuery = "UPDATE PRODUCT_CODE " + "SET DISCOUNT_CODE = ?, DESCRIPTION = ? " + "WHERE PROD_CODE = ?";
-        return connection.prepareStatement(updateQuery);
-    }
-    /**
-     * Преобразует {@link ResultSet} в коллекцию объектов типа {@link ProductCode}
-     * 
-     * @param set {@link ResultSet}, полученный в результате запроса, содержащего 
-     * все поля таблицы PRODUCT_CODE базы данных Sample
-     * @return Коллекция объектов типа {@link ProductCode}
-     * @throws SQLException 
-     */
-    public static Collection<ProductCode> convert(ResultSet set) throws SQLException {
-        /*
-         * TODO #12 Реализуйте метод convert
-         */
-        List<ProductCode> pc = new ArrayList<>();
-        if (set != null) {
-            while (set.next()) {
-                pc.add(new ProductCode(set));
-            }
-            return pc;
-        }
-        throw new IllegalArgumentException("Empty");
-    }
-    /**
-     * Сохраняет текущий объект в базе данных. 
-     * <p>
-     * Если запись ещё не существует, то выполняется запрос типа INSERT.
-     * <p>
-     * Если запись уже существует в базе данных, то выполняется запрос типа UPDATE.
-     * 
-     * @param connection действительное соединение с базой данных
-     * @throws java.sql.SQLException
-     */
-    public void save(Connection connection) throws SQLException {
-        /*
-         * TODO #13 Реализуйте метод convert
-         */
-        Collection<ProductCode> pc = all(connection);
-        PreparedStatement st;
-        if (pc.contains(this)) {
-            st = getUpdateQuery(connection);
-        } else {
-            st = getInsertQuery(connection);
-        }
-        st.setString(1, code);
-        st.setString(2, String.valueOf(discountCode));
-        st.setString(3, description);
-        st.execute();
-    }
-    /**
-     * Возвращает все записи таблицы PRODUCT_CODE в виде коллекции объектов
-     * типа {@link ProductCode}
-     * 
-     * @param connection действительное соединение с базой данных
-     * @return коллекция объектов типа {@link ProductCode}
-     * @throws SQLException 
-     */
-    public static Collection<ProductCode> all(Connection connection) throws SQLException {
-        try (PreparedStatement statement = getSelectQuery(connection)) {
-            try (ResultSet result = statement.executeQuery()) {
-                return convert(result);
-            }
-        }
-    }
 }
